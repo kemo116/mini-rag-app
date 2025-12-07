2dc39aca434123650362660b93ff28ecaad061cb
Kareem Yahya <kareemyahya145@gmail.com>
2025-12-07 21:42:56 +0200

    creating base

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from routes import base 
 
+
+
+load_dotenv(".env")
 app = FastAPI()
-
-@app.get("/welcome")
-def welcome():
-    return {
-        "message": "Welcome to the FastAPI application!"
-        }
+app.include_router(base.router)
---
+++ routes/base.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, APIRouter
+import os
+
+
+
+router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"], 
+)
+
+@router.get("/")
+async def welcome():
+    app_name = os.getenv('APP_NAME')
+    app_version = os.getenv('APP_VERSION')
+    
+    return {
+        "message": "Welcome to the app!",
+        "app_name": app_name,
+        "app_version": app_version
+    }
